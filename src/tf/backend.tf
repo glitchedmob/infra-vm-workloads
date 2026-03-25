dad9a9dc6dfa431a14491cef291f9af01d385ab6
Levi Zitting <me@levizitting.com>
2026-03-25 01:13:39 -0500

    fix: backend state

--- src/tf/backend.tf
+++ src/tf/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "lz-infra-tf-state"
-    key            = "lz-vm-workloads/terraform.tfstate"
+    bucket         = "levizitting-infra-tf-state"
+    key            = "vm-workloads/terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "lz-infra-tflock"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
