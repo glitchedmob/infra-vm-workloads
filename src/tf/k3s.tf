62e5908dacc5ac8a471ded1b5b5b2feefe59d3f6
Levi Zitting <me@levizitting.com>
2026-06-22 21:13:29 -0500

    feat: add grafana github oauth client secret

--- src/tf/k3s.tf
+++ src/tf/k3s.tf
@@ -110,6 +110,13 @@ ephemeral "random_password" "infisical_redis_password" {
   special = false
 }
 
+resource "aws_ssm_parameter" "grafana_github_oauth_client_secret" {
+  name             = "${local.ssm_key_prefix}/grafana-github-oauth-client-secret"
+  type             = "SecureString"
+  value_wo         = "CHANGEME"
+  value_wo_version = 1
+}
+
 resource "aws_ssm_parameter" "infisical_encryption_key" {
   name             = "${local.ssm_key_prefix}/infisical-encryption-key"
   type             = "SecureString"
@@ -168,16 +175,17 @@ resource "ansible_host" "workload" {
   ]
 
   variables = {
-    ansible_host                               = each.value.ipv4_address
-    ansible_user                               = local.vm_user
-    node_name                                  = each.value.node_name
-    ssm_private_key_path                       = module.ssh_key.ssm_path
-    ssm_git_deploy_private_key_path            = module.git_deploy_key.ssm_path
-    ssm_eso_access_key_id_path                 = local.ssm_eso_access_key_id_path
-    ssm_eso_secret_access_key_path             = local.ssm_eso_secret_access_key_path
-    ssm_argocd_github_oauth_client_secret_path = aws_ssm_parameter.argocd_github_oauth_client_secret.name
-    ssm_tailscale_authkey_path                 = "/homelab/headscale/lz-k3s/${each.key}-auth-key"
-    proxmox_vm_role                            = each.value.role
-    ansible_ssh_use_ssh_agent                  = "false"
+    ansible_host                                = each.value.ipv4_address
+    ansible_user                                = local.vm_user
+    node_name                                   = each.value.node_name
+    ssm_private_key_path                        = module.ssh_key.ssm_path
+    ssm_git_deploy_private_key_path             = module.git_deploy_key.ssm_path
+    ssm_eso_access_key_id_path                  = local.ssm_eso_access_key_id_path
+    ssm_eso_secret_access_key_path              = local.ssm_eso_secret_access_key_path
+    ssm_argocd_github_oauth_client_secret_path  = aws_ssm_parameter.argocd_github_oauth_client_secret.name
+    ssm_grafana_github_oauth_client_secret_path = aws_ssm_parameter.grafana_github_oauth_client_secret.name
+    ssm_tailscale_authkey_path                  = "/homelab/headscale/lz-k3s/${each.key}-auth-key"
+    proxmox_vm_role                             = each.value.role
+    ansible_ssh_use_ssh_agent                   = "false"
   }
 }
