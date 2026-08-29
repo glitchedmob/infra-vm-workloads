af0794c9c305d79a8139f4ac27754c5123efbf71
Levi Zitting <me@levizitting.com>
2026-08-28 21:06:32 -0500

    fix: preserve tunnel identity during token updates

--- src/tf/cloudflare.tf
+++ src/tf/cloudflare.tf
@@ -1,21 +1,12 @@
 locals {
-  cloudflare_tunnel_token_ssm_path = "/vm-workloads/lz/infra-vm-workloads/cloudflare-tunnel-token"
-  # Increment to recreate the tunnel and publish its new token to SSM.
-  cloudflare_tunnel_token_version = 1
-}
-
-resource "terraform_data" "cloudflare_tunnel_token_version" {
-  triggers_replace = local.cloudflare_tunnel_token_version
+  cloudflare_tunnel_token_ssm_path    = "/vm-workloads/lz/infra-vm-workloads/cloudflare-tunnel-token"
+  cloudflare_tunnel_token_ssm_version = 1
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "lz_k3s" {
   account_id = var.cloudflare_account_id
   name       = "lz-infra-k8s-apps"
   config_src = "cloudflare"
-
-  lifecycle {
-    replace_triggered_by = [terraform_data.cloudflare_tunnel_token_version]
-  }
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lz_k3s" {
@@ -53,5 +44,5 @@ resource "aws_ssm_parameter" "cloudflare_tunnel_token" {
   name             = local.cloudflare_tunnel_token_ssm_path
   type             = "SecureString"
   value_wo         = data.cloudflare_zero_trust_tunnel_cloudflared_token.lz_k3s.token
-  value_wo_version = local.cloudflare_tunnel_token_version
+  value_wo_version = local.cloudflare_tunnel_token_ssm_version
 }
