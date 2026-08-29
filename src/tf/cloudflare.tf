3288d2830ebf1423513b24bfa686729653152dee
Levi Zitting <me@levizitting.com>
2026-08-28 21:04:51 -0500

    feat: add tunnel token rotation

--- src/tf/cloudflare.tf
+++ src/tf/cloudflare.tf
@@ -1,11 +1,21 @@
 locals {
   cloudflare_tunnel_token_ssm_path = "/vm-workloads/lz/infra-vm-workloads/cloudflare-tunnel-token"
+  # Increment to recreate the tunnel and publish its new token to SSM.
+  cloudflare_tunnel_token_version = 1
+}
+
+resource "terraform_data" "cloudflare_tunnel_token_version" {
+  triggers_replace = local.cloudflare_tunnel_token_version
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "lz_k3s" {
   account_id = var.cloudflare_account_id
   name       = "lz-infra-k8s-apps"
   config_src = "cloudflare"
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.cloudflare_tunnel_token_version]
+  }
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "lz_k3s" {
@@ -40,7 +50,8 @@ data "cloudflare_zero_trust_tunnel_cloudflared_token" "lz_k3s" {
 }
 
 resource "aws_ssm_parameter" "cloudflare_tunnel_token" {
-  name  = local.cloudflare_tunnel_token_ssm_path
-  type  = "SecureString"
-  value = data.cloudflare_zero_trust_tunnel_cloudflared_token.lz_k3s.token
+  name             = local.cloudflare_tunnel_token_ssm_path
+  type             = "SecureString"
+  value_wo         = data.cloudflare_zero_trust_tunnel_cloudflared_token.lz_k3s.token
+  value_wo_version = local.cloudflare_tunnel_token_version
 }
